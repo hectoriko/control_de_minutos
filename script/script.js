03045943caf2298c861ff2e84f37c1aa346b111c
hectoriko <hectoriko@gmail.com>
2020-09-21 16:51:00 +0200

    Primer commit sin minutos

--- script/script.js
+++ script/script.js
@@ -1,53 +1,71 @@
-
-
-
 const botonGuardar = document.getElementById('botonGuardar');
 
 function procesarNombres() { 
   /* Obtener campo nombre alumno */
   const alumnoUno = document.getElementById('campoNombreUno').value;
   const alumnoDos = document.getElementById('campoNombreDos').value;
-  const alumnoTres = document.getElementById('campoNombreTres').value;
+  /* const alumnoTres = document.getElementById('campoNombreTres').value; */
 
-  /* Obtener puntuación alumno */
-  /* const cincoMinUno = document.getElementById('cincoMinUno').value;
-  const diezMinUno = document.getElementById('diezMinUno').value;
-  const quinceMinUno = document.getElementById('quinceMinUno').value; */
+  const seleccionadoTres = document.getElementById("campoNombreTres");
+  const alumnoTres = seleccionadoTres.options[seleccionadoTres.selectedIndex].text;
 
-  
-
-
-  /* Colocar nombres */
+    /* Colocar nombres */
   document.getElementById('nombreUsuarioPrimero').innerHTML = alumnoUno;
   document.getElementById('nombreUsuarioSegundo').innerHTML = alumnoDos;
   document.getElementById('nombreUsuarioTercero').innerHTML = alumnoTres;
 
   /* Colocar minutos */
-   /* document.getElementById('tiempoUsuarioPrimero').innerHTML = cincoMinUno;
-  document.getElementById('tiempoUsuarioSegundo').innerHTML = diezMinUno;
-  document.getElementById('tiempoUsuarioTercero').innerHTML = quinceMinUno; */
+  document.getElementById('tiempoUsuarioPrimero').innerHTML = 15;
+  document.getElementById('tiempoUsuarioSegundo').innerHTML = 10;
+  document.getElementById('tiempoUsuarioTercero').innerHTML = 5;
 
 };
-
-
 
 botonGuardar.onclick = procesarNombres;
 
 
+let sumarMinutos = (nombre, minutos) => {
 
-
-
-
-
-/* class alumno {
-  constructor (name) {
-    this.name = nombre;
-    this.apellido= apellido;
-    this.minutosIniciales= 0;
-    this.minutosAcumlados = 0 + minutosIniciales;
-    this.foto= foto;
-  }
 };
+
+
+const hector {
+  name: 'Héctor',
+  minutosAcumulados: 25;
+};
+
+const ana {
+  name: 'Ana',
+  minutosAcumulados: 15;
+
+}
+
+
+/*
+class alumno {
+  constructor (nombre, minutosAcumulados) {
+    this.nombre = nombre;
+    this.minutosAcumulados = minutosAcumulados;
+  }
+  
+  get nombre() {
+    return this.nombre;
+  }
+
+  get minutosAcumulados() {
+    return this.minutosAcumulados;
+  }
+  
+};
+} */
+
+
+
+
+
+
+
+
 
 
 
